58c10b6b315e63de98375409b275104c2c017127
Julian Hisdal Nymark <julianymark@gmail.com>
2014-03-07 17:53:33 +0100

    now with delta time!

--- funler/src/funler/Funler.java
+++ funler/src/funler/Funler.java
@@ -2,16 +2,47 @@ package funler;
 
 import processing.core.PApplet;
 
-
 public class Funler extends PApplet {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public static final boolean DEBUG = true;
+
+	public static final int TEXT_SIZE = 22;
+	
+	private int timer_start = 0;
+	private int timer_stop = 0;
+
 	public void setup() {
-	    size(123, 321);
+		size(900, 800);
+		textSize(TEXT_SIZE);
 	}
 
+	
+	
 	public void draw() {
+		timer_stop = millis();
+		int delta_time = timer_stop - timer_start;
+		draw( delta_time / 1000f );
+		timer_start = millis();
 	}
 	
+	public void draw( float dt ){
+		if (DEBUG) {
+			background(100,200,255);
+			
+			println(dt);
+			
+			text(frameRate, width - (5*TEXT_SIZE), height - TEXT_SIZE);
+		}
+		else {
+			
+		}		
+	}
+
 	public static void main(String _args[]) {
 		PApplet.main(new String[] { funler.Funler.class.getName() });
 	}
