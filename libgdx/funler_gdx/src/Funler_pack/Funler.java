090b0b2d3e2933797cc1211811caa94b34e62779
Perimus Persijn <persijn.d.k@gmail.com>
2014-03-12 19:59:50 +0100

    more changes in java files

--- libgdx/funler_gdx/src/Funler_pack/Funler.java
+++ libgdx/funler_gdx/src/Funler_pack/Funler.java
@@ -4,20 +4,13 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.Texture.TextureFilter;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 
 public class Funler implements ApplicationListener {
 	private OrthographicCamera camera;
 	private SpriteBatch batch;
-	private Texture texture;
-	private Sprite sprite;
-	
 	@Override
 	public void create() {		
 		float w = Gdx.graphics.getWidth();
