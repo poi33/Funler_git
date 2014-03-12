13f962ed0b5990959f03b40085fda1f11542b442
Perimus Persijn <persijn.d.k@gmail.com>
2014-03-12 18:54:40 +0100

    Porting it to a new game engine. Lets see if this makes any thing better :P heyo

---
+++ libgdx/funler_gdx-desktop/src/Funler_pack/Main.java
@@ -0,0 +1,15 @@
+package Funler_pack;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "funler_gdx";
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new Funler(), cfg);
+	}
+}
