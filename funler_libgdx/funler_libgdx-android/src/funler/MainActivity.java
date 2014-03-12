979750d8bcb9238d4a82f74e7dbdb817b84f6062
Julian Hisdal Nymark <julianymark@gmail.com>
2014-03-12 18:27:04 +0100

    libgdx project

---
+++ funler_libgdx/funler_libgdx-android/src/funler/MainActivity.java
@@ -0,0 +1,13 @@
+package funler;
+
+public class MainActivity extends AndroidApplication {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
+        cfg.useGL20 = false;
+        
+        initialize(new Funler(), cfg);
+    }
+}
