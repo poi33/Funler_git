90171788631c8c421de65337b292c560a97987fc
Julian Hisdal Nymark <julianymark@gmail.com>
2014-03-19 12:45:16 +0100

    nothing important

--- libgdx/funler_gdx/src/Funler_pack/Funler.java
+++ libgdx/funler_gdx/src/Funler_pack/Funler.java
@@ -42,7 +42,8 @@ public class Funler implements ApplicationListener {
 	@Override
 	//Draw loop
 	public void render() {		
-		float delataT = Gdx.graphics.getDeltaTime();
+		float deltaT = Gdx.graphics.getDeltaTime();
+		
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
